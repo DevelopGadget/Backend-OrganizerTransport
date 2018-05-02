4b677e47d10343fbf62006bc4bf1cc093fbd4b61
FernanPro18 <fernandoaraujo081@gmail.com>
2018-05-02 07:43:42 -0500

    Incio

---
+++ OrganizerTransport/Models/Dia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizerTransport.Models
+{
+    public class Horario
+    {
+        public DateTime Dia { get; set; }
+        public int Ida { get; set; }
+        public int Venida { get; set; }
+    }
+}
