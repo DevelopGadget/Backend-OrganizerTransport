1d4a28ddab69460a3c457a2c52884c051b6ef882
FernanPro18 <fernandoaraujo081@gmail.com>
2018-05-10 18:23:08 -0500

    Arreglos --otra

--- Startup.cs
+++ Startup.cs
@@ -12,36 +12,32 @@ using OrganizerTransport.Interfaces;
 using OrganizerTransport.Models;
 using OrganizerTransport.Repositorios;
 
-namespace OrganizerTransport
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace OrganizerTransport {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.Configure<Settings>(o => { o.configuration = (IConfiguration)Configuration; });
-            services.AddTransient<ISaldo, Saldo_Repositorio>();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ();
+            services.Configure<Settings> (o => { o.configuration = (IConfiguration) Configuration; });
+            services.AddTransient<ISaldo, Saldo_Repositorio> ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
-            app.UseCors(data =>
-            data.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-            app.UseMvc(Routes => {
-                Routes.MapRoute(
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseCors (data =>
+                data.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ());
+            app.UseMvc (Routes => {
+                Routes.MapRoute (
                     name: "Default",
                     template: "{controller=Home}/{action=Index}/{id?}"
                 );
